c807a3a315712f0f9d268103cce1157201dc320e
mzkmnk <sma0729mex@gmail.com>
2025-03-17 08:15:11 +0900

    wippppp

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import { serve } from '@hono/node-server'
+import { createNodeWebSocket } from '@hono/node-ws'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const { injectWebSocket,upgradeWebSocket } = createNodeWebSocket({app});
 
-serve({
+app.get('/ws', upgradeWebSocket((c) => {
+  return {
+    onMessage: (event,ws) => {
+      console.log(`Message from client: ${event.data}`)
+
+      ws.send('Hello from server')
+    }
+  }
+}))
+
+const server = serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
+
+
+injectWebSocket(server);
